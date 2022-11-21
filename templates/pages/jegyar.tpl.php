c43c298e826df02ca21d29f8f9e634dc36667ecc
dariosuha1981 <118468670+dariosuha1981@users.noreply.github.com>
2022-11-21 13:53:52 +0100

    árjegyzék módosítás

--- templates/pages/jegyar.tpl.php
+++ templates/pages/jegyar.tpl.php
@@ -2,12 +2,7 @@
     <caption>
         <h1>Áraink</h1>
     </caption>
-    <tr>
-        <th>Napijegy</th>
-        <th>10 alkalmas bérlet</th>
-        <th>Havi bérlet</th>
-        <th>Ring használat</th>
-    </tr>
+    
     <tr>
         <td>2000 FT</td>
         <td>8000 Ft</td>

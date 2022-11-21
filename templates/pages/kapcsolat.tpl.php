4549dd02c0073aa8794dc0238cc1ba309af2c8c4
dariosuha1981 <118468670+dariosuha1981@users.noreply.github.com>
2022-11-21 13:46:32 +0100

    név kiegészítés

--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -1,5 +1,5 @@
 <h2>Adatok:</h2>
-<p class="vezetok">Ügyvezetők: <br><strong>Oroszi Tibor <br>Suha Dárió<br>Sebestyén Anita</strong> <br> E-mail: <strong>never_skip_the_leg_days@colosseumgym.hu</strong></p>
+<p class="vezetok">Ügyvezetők: <br><strong>Oroszi Tibor Tamás <br>Suha Dárió<br>Sebestyén Anita</strong> <br> E-mail: <strong>never_skip_the_leg_days@colosseumgym.hu</strong></p>
 <div class="terkep">
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1323.9621194533897!2d19.706438576866876!3d46.91227040401812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da23b5bf5d05%3A0x2b92cbe4fc648e18!2sColosseum%20Fitness!5e0!3m2!1shu!2shu!4v1667749574559!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 <br>

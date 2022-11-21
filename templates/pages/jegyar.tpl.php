238c49dffc10ee33e1acfb96b9d2dfcc671b29bd
dariosuha1981 <118468670+dariosuha1981@users.noreply.github.com>
2022-11-21 13:54:25 +0100

    visszaállítás

--- templates/pages/jegyar.tpl.php
+++ templates/pages/jegyar.tpl.php
@@ -2,7 +2,12 @@
     <caption>
         <h1>Áraink</h1>
     </caption>
-    
+    <tr>
+        <th>Napijegy</th>
+        <th>10 alkalmas bérlet</th>
+        <th>Havi bérlet</th>
+        <th>Ring használat</th>
+    </tr>
     <tr>
         <td>2000 FT</td>
         <td>8000 Ft</td>
